a07fd6e637924c14a216cf271ef91ad687bc927d
whoiscc <correctizer@gmail.com>
2019-01-18 23:42:40 -0800

    Initial Commit

---
+++ Flawed/Flawed.h
@@ -0,0 +1,19 @@
+//
+//  Flawed.h
+//  Flawed
+//
+//  Created by Cowsay on 2019/1/18.
+//  Copyright © 2019 Cowsay. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Flawed.
+FOUNDATION_EXPORT double FlawedVersionNumber;
+
+//! Project version string for Flawed.
+FOUNDATION_EXPORT const unsigned char FlawedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flawed/PublicHeader.h>
+
+
